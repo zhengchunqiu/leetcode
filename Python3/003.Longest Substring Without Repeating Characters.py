d0223b2328c0ae95a098c33d170f553432cdc31a
zhengchunqiu <37128157+zhengchunqiu@users.noreply.github.com>
2018-07-17 20:56:29 +0800

    Update 003.Longest Substring Without Repeating Characters.py

--- Python3/003.Longest Substring Without Repeating Characters.py
+++ Python3/003.Longest Substring Without Repeating Characters.py
@@ -1,3 +1,47 @@
+#1.暴力 596ms 13.06%
+class Solution(object):
+    def lengthOfLongestSubstring(self,s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_len = 0
+        length = len(s)
+        if (length == 0 or length == 1):
+            max_len = length
+        
+        lens=length-1
+        for i in range(lens):
+            for j in range(i + 1, length):
+                if s[j] in s[i:j]:
+                    if j - i > max_len:
+                        max_len = j - i
+                    break
+                elif j == lens:
+                    if max_len < length- i :
+                        max_len = length-i
+        return max_len
+#2.字典
+class Solution(object):
+    def lengthOfLongestSubstring(self,s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic={}
+        maxLength=currMax=0
+        length=len(s)
+        for i in range(length):
+            if s[i] in dic and i-dic[s[i]]-1<currMax:
+                if maxLength<currMax:
+                    maxLength=currMax
+                currMax=i-dic[s[i]]-1
+            currMax+=1
+            dic[s[i]]=i
+        return maxLength if currMax<maxLength else currMax
+
+
+####################################################################
 if 'b' in 'ab':
     print('OK')
 
